986c0a94fb1bb5b26022bfbf267c623263461c97
João <joaoantoniolaraujo@gmail.com>
2023-10-18 20:50:49 -0300

    2.9

---
+++ lib/splashScreen.dart
@@ -0,0 +1,32 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:desafio/main.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/images/carregamento.json'),
+      nextScreen: LoginApp(),
+      splashIconSize: 250,
+      duration: 3000,
+      backgroundColor: Color.fromARGB(255, 86, 97, 255),
+      splashTransition: SplashTransition.scaleTransition,
+      
+    );
+  }
+}
